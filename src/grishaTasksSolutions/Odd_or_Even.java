ed074ef585ad6427655cbd100c3a83102ffb79b5
gregoryreydler <grisharydler@gmail.com>
2024-11-15 10:34:57 -0500

    New Task Added

---
+++ src/grishaTasksSolutions/Odd_or_Even.java
@@ -0,0 +1,12 @@
+package grishaTasksSolutions;
+
+public class Odd_or_Even {
+
+    /*
+    Write  a method which can identifies given number is even or odd
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+
+     */
+}
