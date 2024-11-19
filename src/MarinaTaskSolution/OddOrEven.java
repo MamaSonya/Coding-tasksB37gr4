1849637a43c7d55b8d7b31d07c22237593e4eb6c
myndee82 <marinareydler13@gmail.com>
2024-11-18 21:47:57 -0500

    First Week Task Solution

--- src/MarinaTaskSolution/OddOrEven.java
+++ src/MarinaTaskSolution/OddOrEven.java
@@ -2,13 +2,30 @@ package MarinaTaskSolution;
 
 public class OddOrEven {
 
+    public static String result (int num){
+
+        if(num % 2 == 0){
+            return "Even";
+        }else{
+            return "Odd";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(result(5));
+        System.out.println(result(6));
+    }
+}
+
+
+
+
      /*
-    Write  a method which can identifies given number is even or odd
+    Write  a method which can identify given number is even or odd:
         EX:
         identify(5) ->  "Odd"
         identify(6) ->  "Even"
 
      */
-    //Mitra will share the solution
 
-}
+
