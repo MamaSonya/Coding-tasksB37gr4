11a2d32647d8858777a8178b7bf9c8ee02776e67
gregoryreydler <grisharydler@gmail.com>
2024-11-19 01:03:32 -0500

    New Task Added

--- src/grishaTasksSolutions/Odd_or_Even.java
+++ src/grishaTasksSolutions/Odd_or_Even.java
@@ -2,6 +2,25 @@ package grishaTasksSolutions;
 
 public class Odd_or_Even {
 
+
+        public static String determine (int number) {
+            // Check if the number is even or odd and return the result
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+        }
+
+        public static void main(String[] args) {
+            // Test the method with different numbers
+            System.out.println(determine(5)); // Output: Odd
+            System.out.println(determine(6)); // Output: Even
+        }
+    }
+
+
+
     /*
     Write  a method which can identifies given number is even or odd
         EX:
@@ -9,4 +28,4 @@ public class Odd_or_Even {
         identify(6) ->  "Even"
 
      */
-}
+
