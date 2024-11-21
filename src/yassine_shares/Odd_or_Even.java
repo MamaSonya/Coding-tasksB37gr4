a454298e14e50b474afaa36200e8aeafbd6edded
yassinesacky <yassinesacky@gmail.com>
2024-11-21 18:59:56 -0500

    commit 1st week task - solution

--- src/yassine_shares/Odd_or_Even.java
+++ src/yassine_shares/Odd_or_Even.java
@@ -1,12 +1,27 @@
 package yassine_shares;
 
-public class Odd_or_Even {
-
-    /*
+/*
     Write  a method which can identifies given number is even or odd
         EX:
         identify(5) ->  "Odd"
         identify(6) ->  "Even"
 
      */
+public class Odd_or_Even {
+
+    public static String oddOrEvenIdentifier(int num) {
+
+        if (num % 2 == 0)
+            return "even";
+        else {
+            return "odd";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(oddOrEvenIdentifier(5));
+        System.out.println(oddOrEvenIdentifier(6));
+    }
+
 }
