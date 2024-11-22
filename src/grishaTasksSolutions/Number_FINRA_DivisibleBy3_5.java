6c7f28bb2259a5264d5886c9a7c63ef34f945c48
gregoryreydler <grisharydler@gmail.com>
2024-11-22 14:53:31 -0500

    Added FINRA class using "for" loop method

--- src/grishaTasksSolutions/Number_FINRA_DivisibleBy3_5.java
+++ src/grishaTasksSolutions/Number_FINRA_DivisibleBy3_5.java
@@ -8,7 +8,7 @@ public class Number_FINRA_DivisibleBy3_5 {
     for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
   */
 // 1 2 FIN 4 RA FIN 7 8 FIN RA 11 FIN 13 14 FINRA 16 17 FIN 19 RA FIN 22 23 FIN RA 26 FIN 28 29 FINRA
-// Silvia is a volunteer 
+// Silvia is a volunteer
 
 
     public static void printFinRa() {
