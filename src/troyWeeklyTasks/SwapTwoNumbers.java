d4d86fa4f7b17d58275e7936ecd4aafd6900017d
TroyYC <troy@cydeo.com>
2024-12-06 13:07:45 -0500

    week4 task added

--- src/troyWeeklyTasks/SwapTwoNumbers.java
+++ src/troyWeeklyTasks/SwapTwoNumbers.java
@@ -2,12 +2,20 @@ package troyWeeklyTasks;
 
 public class SwapTwoNumbers {
      /*
+
+
     Swap two variable values without using a third variable
 
     int a = 5;
     int b = 7;
 
-    a = 7;
-    b = 5;
+   System.out.println("a = " + a); //=> a = 7
+   System.out.println("b = " + b); //=> b = 5
+
+
+
+
+
+    Marina is volunteer Grishe will help her
      */
 }
