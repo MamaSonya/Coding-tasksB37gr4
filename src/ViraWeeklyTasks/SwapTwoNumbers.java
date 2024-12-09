06402f1ba89c7db36dfdd7dfb08ba86c7811d8cd
virakaryta <virakaryta14@gmail.com>
2024-12-09 13:29:50 -0500

    add task solution

---
+++ src/ViraWeeklyTasks/SwapTwoNumbers.java
@@ -0,0 +1,40 @@
+package AseyarWeeklyTasksRecent.ViraWeeklyTasks;
+
+public class SwapTwoNumbers {
+    /*
+    Swap two variable values without using a third variable
+
+    int a = 5;
+    int b = 7;
+
+   System.out.println("a = " + a); //=> a = 7
+   System.out.println("b = " + b); //=> b = 5
+
+     */
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+
+        a = a + b; // a = 12
+        b = a - b; // b = 5
+        a = a - 5; // a = 7
+
+        System.out.println("a = " + a); //=> a = 7
+        System.out.println("b = " + b); //=> b = 5
+    }
+
+   /*
+   OPTION 2
+   Swap two variable values with a third variable
+        int a = 5;
+        int b = 7;
+        int temp;
+
+        temp = a;  // temp = 5, a = 5
+        a = b;     // a = 7, b = 7
+        b = temp;  // a = 7, b = 5
+        System.out.println("a = " + a); //=> a = 7
+        System.out.println("b = " + b); //=> b = 5
+    */
+}
