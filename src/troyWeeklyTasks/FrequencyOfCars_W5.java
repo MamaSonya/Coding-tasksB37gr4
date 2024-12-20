6a13675431610b99512ec49ebdf5dc028af73846
TroyYC <troy@cydeo.com>
2024-12-20 11:13:43 -0500

    week5 solution added

--- src/troyWeeklyTasks/FrequencyOfCars_W5.java
+++ src/troyWeeklyTasks/FrequencyOfCars_W5.java
@@ -11,4 +11,27 @@ public class FrequencyOfCars_W5 {
         Andrea is volunteer
     */
 
+    public static String frequencyOfChars1(String str) {
+        String expectedResult = "";
+        int j = 0;
+        while (j < str.length()) {//aaabbbcccdddEEEFFF
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {//aaabbbcccdddEEEFFF
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;//3
+                }
+            }
+            expectedResult += str.charAt(j) + "" + count;//a3b3
+            str = str.replace("" + str.charAt(j), "");
+            System.out.println(str);
+
+        }
+        return expectedResult;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(frequencyOfChars1("aaabbbcccdddEEEFFF"));
+    }
+
 }
