94fc8518cb4e2946fa5098eff8bc5bf594f6a390
TroyYC <troy@cydeo.com>
2025-01-03 14:23:31 -0500

    week5 solution and week6 task added

---
+++ src/troyWeeklyTasks/RemoveDuplicatesW6.java
@@ -0,0 +1,11 @@
+package troyWeeklyTasks;
+
+public class RemoveDuplicatesW6 {
+
+     /*
+    Write function that can remove the duplicated values from String
+            Ex: removeDup("AAABBBCCC") ==> ABC
+     */
+
+    //Vira is volunteer
+}
